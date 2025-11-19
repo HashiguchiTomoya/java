8ae89d8582d19abf3a5ebbd7a21eefb8fe760a86
HashiguchiTomoya <hashiguchi.ty@wain.co.jp>
2025-11-19 17:22:42 +0900

    Delete 026_replication directory

--- 026_replication/replication.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS example;
--- レプリケーション用のDB
-CREATE DATABASE example;
-USE example;
-
--- Slaveアカウントを作成
-CREATE user 'repl'@'192.168.33.20' identified by 'SLave-password';
-GRANT replication slave on *.* to 'repl'@'192.168.33.20';
